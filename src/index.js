45ce18e7a44cf6b7458602c6161d7cb73c1536e1
Techmile360 <techmile360@gmail.com>
2026-06-08 07:38:14 -0700

    Revert "Set Content-Encoding gzip on .unityweb assets for Unity startup"
    This reverts commit cabfa827f8baadc3ea3cf6c1b9b31c1500796464.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@
  */
 export default {
   async fetch(request, env) {
-    const response = await env.ASSETS.fetch(request);
-    const path = new URL(request.url).pathname;
-    // Unity .unityweb files are gzip-compressed; header enables faster startup.
-    if (path.endsWith(".unityweb")) {
-      const headers = new Headers(response.headers);
-      headers.set("Content-Encoding", "gzip");
-      return new Response(response.body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers,
-      });
-    }
-    return response;
+    return env.ASSETS.fetch(request);
   },
 };
